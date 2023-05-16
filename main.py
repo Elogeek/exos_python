77c91107213ec995258e0177757e5ca40818a270
elogeek <elodiechristin@gmail.com>
2023-05-16 11:34:13 +0200

    objects in python

---
+++ main.py
@@ -0,0 +1,12 @@
+
+
+player1 = Player("Cookies", 20, 3)
+#print("Pseudo: ", player1.get_pseudo())
+#player1.damage(5)
+#print("Il vous reste maintenant", player1.get_health(), "points de vie!")
+player2 = Player("Otaku45", 35, 5)
+
+player1.attack_player(player2)
+print(player1.get_pseudo(), "attaque", player2.get_pseudo())
+print("Bienvenue au joueur", player1.get_pseudo(), "/ Points de vie:", player1.get_health(), "/ Attaque:", player1.get_attack_value())
+print("Bienvenue au joueur", player2.get_pseudo(), "/ Points de vie:", player2.get_health(), "/ Attaque:", player2.get_attack_value())
---
+++ player.py
@@ -0,0 +1,23 @@
+class Player:
+
+    # Constructor
+    def __init__(self, pseudo, health, attack):
+        self.pseudo = pseudo
+        self.health = health
+        self.attack = attack
+        print("Bienvenue au joueur", pseudo, "/ Points de vie:", health, "/ Attaque:", attack)
+
+    def get_pseudo(self):
+        return self.pseudo
+
+    def get_health(self):
+        return self.health
+
+    def get_attack_value(self):
+        return self.attack
+    def damage(self, damage):
+        self.health -= damage
+        #print("Aiiie...vous venez de subir", damage, "dégats!")
+    # Attack another player
+    def attack_player(self, target_player):
+        target_player.damage(self.attack)
